9b04ee42cb8746489512b4026d42e21d023eb7ae
VaderXI <aronjoseph8008@gmail.com>
2026-04-04 20:15:50 +0530

    fix helmet blocking cors

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,21 @@ import shareRouter from './routes/share.js'
 dotenv.config()
 
 const app = express()
-app.use(helmet())
+
 app.use(cors({
   origin: ['http://localhost:5173', 'https://veilora.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
+
 app.options('*', cors())
+
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+  crossOriginOpenerPolicy: false
+}))
+
 app.use(express.json())
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }))
 
